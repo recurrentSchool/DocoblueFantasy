9005c27a309eab44f83497b283ab16eec1224dd1
6D166 <6D166@DESKTOP-L5BBK72>
2019-10-07 17:00:35 +0900

    LoginServlet更新しました。

--- docobluefantasy/src/servlet/LoginServlet.java
+++ docobluefantasy/src/servlet/LoginServlet.java
@@ -2,41 +2,52 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class LoginServlet
- */
+import bean.User;
+import model.LoginLogic;
+
+
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	}//doGET
+
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		request.setCharacterEncoding("UTF-8");
+
+		String name = request.getParameter("name");
+		String pass = request.getParameter("pass");
+
+		User user = new User(name, pass);
+
+		LoginLogic bo = new LoginLogic();
+
+		boolean result = bo.executeLogin(user);
+
+		HttpSession session = request.getSession();
+		session.setAttribute("user",user );
+
+		if (result==true) {
+			//loginResult.jspへフォワード
+			RequestDispatcher dis = request.getRequestDispatcher("/main.jsp");
+			dis.forward(request, response);
+		}
+		else {
+			response.sendRedirect("/login.jsp");
+		}
+
+	}//doPOST
 
 }
