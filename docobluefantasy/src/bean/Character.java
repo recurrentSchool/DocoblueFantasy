9597cfaa0c566128cee8dec325ca9b3b8023ae11
6D157 <6D157@DESKTOP-RAJ1UAT>
2019-10-07 11:57:27 +0900

    Bean完成

--- docobluefantasy/src/bean/Character.java
+++ docobluefantasy/src/bean/Character.java
@@ -2,4 +2,46 @@ package bean;
 
 public class Character {
 
+	private String name;
+	private String rarity;
+	private int attack;
+	private int hp;
+	private String skill;
+	private int evalution;
+
+	Character(String name,String rarity,int attack,int hp,String skill,int evalution){
+
+		this.name = name;
+		this.rarity = rarity;
+		this.attack = attack;
+		this.hp = hp;
+		this.skill = skill;
+		this.evalution = evalution;
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getRarity() {
+		return rarity;
+	}
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public String getSkill() {
+		return skill;
+	}
+
+	public int getEvalution() {
+		return evalution;
+	}
+
 }
--- docobluefantasy/src/bean/User.java
+++ docobluefantasy/src/bean/User.java
@@ -1,5 +1,38 @@
 package bean;
 
 public class User {
+	private int admin;
+	private String name;
+	private String pass;
+	private int billing;
+
+	User(String name,String pass){
+		this.name = name;
+		this.pass = pass;
+	}
+
+	User(int admin,String name, String pass,int billing){
+		this.admin = admin;
+		this.name = name;
+		this.pass = pass;
+		this.billing = billing;
+	}
+
+	public int getAdmin() {
+		return admin;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public int getBilling() {
+		return billing;
+	}
+
 
 }
